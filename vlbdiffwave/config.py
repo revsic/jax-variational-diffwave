849c0364308ee9f0422ec48ed46d89795e98bfc6
revsic <revsic99@gmail.com>
2021-08-08 23:19:16 +0900

    config: Add hop param

--- vlbdiffwave/config.py
+++ vlbdiffwave/config.py
@@ -1,7 +1,13 @@
 class Config:
     """Configuration for VLB-DiffWave implementation.
     """
-    def __init__(self):
+    def __init__(self, hop: int):
+        """Initializer.
+        Args:
+            hop: hop-size from STFT.
+        """
+        self.hop = hop
+
         # embedding config
         self.embedding_size = 128
         self.embedding_proj = 512
@@ -12,8 +18,6 @@ class Config:
         self.upsample_strides = [16, 1]
         self.upsample_kernels = [32, 3]
         self.upsample_layers = 2
-        # computed hop size
-        self.hop = self.upsample_stride[0] ** self.upsample_layers
 
         # block config
         self.channels = 64
