987ac5166f8451b2cfe97ec4a7ef922e4e2a490e
revsic <revsic99@gmail.com>
2021-08-10 21:50:02 +0900

    logsnr: Move to setup

--- vlbdiffwave/logsnr.py
+++ vlbdiffwave/logsnr.py
@@ -46,7 +46,20 @@ class LogSNR(nn.Module):
     initial_gamma_min: float = -10.
     initial_gamma_gap: float = 20.
 
-    @nn.compact
+    def setup(self):
+        """Setup layers.
+        """
+        # boundary
+        self.gamma_min = self.param(
+            'gamma_min', constant(self.initial_gamma_min), [])
+        # force positive
+        self.gamma_gap = nn.softplus(
+            self.param('gamma_gap', constant(self.initial_gamma_gap), []))
+        # projector
+        self.proj1 = PosDense(channels=1)
+        self.proj2 = PosDense(channels=self.internal)
+        self.proj3 = PosDense(channels=1)
+
     def __call__(self, inputs: jnp.ndarray) -> jnp.ndarray:
         """Compute logSNR from continuous timesteps.
         Args:
@@ -55,24 +68,18 @@ class LogSNR(nn.Module):
             [float32; [B]], logSNR.
             [float32; [B]], normalized -logSNR in [0, 1].
         """
-        # boundary
-        gamma_min = self.param(
-            'gamma_min', constant(self.initial_gamma_min), [])
-        # force positive
-        gamma_gap = nn.softplus(
-            self.param('gamma_gap', constant(self.initial_gamma_gap), []))
         # [B + 2], add terminal point
         x = jnp.concatenate([[0., 1.], inputs], axis=0) 
         # [B + 2, 1]
-        l1 = PosDense(channels=1)(x[:, None])
+        l1 = self.proj1(x[:, None])
         # [B + 2, C]
-        l2 = nn.sigmoid(PosDense(channels=self.internal)(l1))
+        l2 = nn.sigmoid(self.proj2(l1))
         # [B + 2], learned scheduler
-        sched = jnp.squeeze(l1 + PosDense(channels=1)(l2), axis=-1)
+        sched = jnp.squeeze(l1 + self.proj3(l2), axis=-1)
         # [], [], [B]
         s0, s1, sched = sched[0], sched[1], sched[2:]
         # [B], normalized -logSNR
         norm_nlogsnr = (sched - s0) / (s1 - s0)        
         # [B], boundary matching
-        nlogsnr = gamma_min + gamma_gap * norm_nlogsnr
+        nlogsnr = self.gamma_min + self.gamma_gap * norm_nlogsnr
         return -nlogsnr, norm_nlogsnr
