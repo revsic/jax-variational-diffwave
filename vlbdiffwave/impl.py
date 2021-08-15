4494ce41f52f7b1f5cb8f57375c1e8768ccd5fd5
revsic <revsic99@gmail.com>
2021-08-15 11:49:46 +0000

    impl: Add denoising procedure

--- vlbdiffwave/impl.py
+++ vlbdiffwave/impl.py
@@ -57,14 +57,14 @@ class VLBDiffWave:
     def apply(self,
               param: flax.core.frozen_dict.FrozenDict,
               signal: jnp.ndarray,
-              time: jnp.ndarray,
-              mel: jnp.ndarray) -> Tuple[jnp.ndarray, Tuple[jnp.ndarray, jnp.ndarray]]:
+              mel: jnp.ndarray,
+              time: jnp.ndarray) -> Tuple[jnp.ndarray, Tuple[jnp.ndarray, jnp.ndarray]]:
         """Denoise signal w.r.t timestep on mel-condition.
         Args:
             param: model parameters.
             signal: [float32; [B, T]], noised signal.
-            timestep: [float32; [B]], current timestep.
             mel: [float32; [B, T // H, M]], mel-spectrogram.
+            timestep: [float32; [B]], current timestep.
         Returns:
             noise: [float32; [B, T]], estimated noise.
             alpha_sq, sigma_sq: [float32; [B]], signal, noise rates. 
@@ -74,6 +74,37 @@ class VLBDiffWave:
         # [B, T]
         noise = self.diffwave.apply(param['diffwave'], signal, norm_nlogsnr, mel)     
         return noise, (alpha_sq, sigma_sq)
+
+    def denoise(self,
+                param: flax.core.frozen_dict.FrozenDict,
+                signal: jnp.ndarray,
+                mel: jnp.ndarray,
+                t: jnp.ndarray,
+                s: Optional[jnp.ndarray] = None) -> jnp.ndarray:
+        """Denoise process.
+        Args:
+            param: model parameters.
+            signal: [float32; [B, T]], input signal.
+            mel: [float32; [B, T // H, M]], mel-spectrogram.
+            t: [float32; [B]], target time in range[0, 1].
+            s: [float32; [B]], start time in range[0, 1], s < t.
+                if s is None, compute p(z_t), otherwise, p(z_s|z_t).
+        Returns:
+            [float32; [B, T]], denoised signal.
+        """
+        # [B, T], [B], [B]
+        noise, (alpha_sq, sigma_sq) = self.model.apply(param, signal, t, mel)
+        if s is not None:
+            # [B] x 2
+            _, _, alpha_sq_s, sigma_sq_s = self.model.snr(param, s)
+            # [B]
+            alpha_sq_tbars = alpha_sq / alpha_sq_s
+            sigma_sq_tbars = sigma_sq - alpha_sq_tbars * sigma_sq_s
+            # [B]
+            alpha_sq = alpha_sq_tbars
+            sigma_sq = sigma_sq_tbars ** 2 / sigma_sq
+        # denoise
+        return 1 / jnp.sqrt(alpha_sq) * (signal - jnp.sqrt(sigma_sq) * noise)
 
     def diffusion(self,
                   param: flax.core.frozen_dict.FrozenDict,
