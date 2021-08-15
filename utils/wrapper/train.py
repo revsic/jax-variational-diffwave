2bda7a537bcf5feec5d0126f7003719660bf0cd3
revsic <revsic99@gmail.com>
2021-08-15 12:51:49 +0000

    train: Fix dlogsnr

--- utils/wrapper/train.py
+++ utils/wrapper/train.py
@@ -21,15 +21,15 @@ class TrainWrapper:
                      params: flax.core.frozen_dict.FrozenDict,
                      signal: jnp.ndarray,
                      noise: jnp.ndarray,
-                     timestep: jnp.ndarray,
-                     mel: jnp.ndarray) -> jnp.ndarray:
+                     mel: jnp.ndarray,
+                     timestep: jnp.ndarray) -> jnp.ndarray:
         """Compute noise estimation loss.
         Args:
             params: model prameters.
             signal: [float32; [B, T]], speech signal.
             noise: [float32; [B, T]], noise signal.
-            timestep: [float32; [B]], input timestep.
             mel: [float32; [B, T // H, M]], mel-spectrogram.
+            timestep: [float32; [B]], input timestep.
         Returns:
             [float32; []], loss value.
         """
@@ -40,17 +40,15 @@ class TrainWrapper:
         # [B]
         mse = jnp.square(noise - estim).sum(axis=-1)
         # [B], dlog-SNR/dt
-        dlogsnr, _ = jax.grad(
+        dlogsnr = jax.grad(
             # lifting
-            lambda t: self.model.logsnr.apply(params['logsnr'], t),
-            # compute gradient only on log-SNR w.r.t. timestep
-            argnums=1, has_aux=True)(timestep)
+            lambda t: self.model.logsnr.apply(params['logsnr'], t)[0].sum())(timestep)
         # [B]
         loss = -0.5 * dlogsnr * mse
         # []
         loss = loss.mean()
         # set loss to the memory
-        self.model.logsnr.memory = loss
+        self.model.logsnr.pipeline.memory = loss
         return loss
 
     def gradient(self,
